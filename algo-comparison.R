e03cc5c089767e89a6105f7c35681b081de84b5b
Thomas Mailund <mailund@birc.au.dk>
2018-10-15 16:14:36 +0200

    More experimentation with constructor functions

--- algo-comparison.R
+++ algo-comparison.R
@@ -93,4 +93,68 @@ rbind(app_vector_times, #app_llist_times,
     ggplot(aes(x = n, y = times, colour = app)) +
     geom_line()
 
+process_arg <- function(argument) {
+    error_msg <- glue::glue(
+        "The constructor argument is malformed.\n",
+        "The expression {deparse(argument)} should either be ",
+        "a bare symbol or on the form 'variable : type'."
+    )
+    if (rlang::is_lang(argument)) {
+        if (argument[[1]] != ":") {
+            stop(simpleError(error_msg, call = argument))
+        }
+        arg <- rlang::quo_name(argument[[2]])
+        type <- rlang::quo_name(argument[[3]])
+        tibble::tibble(arg = arg, type = type)
+    } else if (rlang::is_symbol(argument)) {
+        arg <- rlang::quo_name(argument)
+        tibble::tibble(arg = arg, type = NA)
+    } else {
+        stop(simpleError(error_msg, call = argument))
+    }
+}
 
+#' Construct a tibble from all the arguments of a constructor
+#'
+#' @param constructor_arguments The arguments provided in the constructor
+#'   specification
+#' @return The arguments represented as a tibble. The first column contain
+#'   argument names, the second their types.
+process_arguments <- function(constructor_arguments) {
+    dplyr::bind_rows(purrr::map(as.list(constructor_arguments), process_arg))
+}
+
+new_process_constructor_function <- function(constructor, data_type_name, env) {
+    constructor_name <- rlang::quo_name(constructor[[1]])
+    constructor_arguments <- process_arguments(constructor[-1])
+
+    # create the constructor function
+    constructor <- function() {
+        args <- rlang::as_list(environment())
+
+        # type check!
+        for (i in seq_along(args)) {
+            arg <- args[[constructor_arguments$arg[i]]]
+            type <- constructor_arguments$type[i]
+            if (!rlang::is_na(type) && !inherits(arg, type)) {
+                # FIXME: how do I get a "bad error message" error here? id:3 gh:42 ic:gh
+                error_msg <- glue::glue(
+                    "The argument {arg} is of type {class(arg)} ",
+                    "but should be of type {type}."
+                )
+                stop(simpleError(error_msg, call = match.call()))
+            }
+        }
+
+        structure(args, constructor = constructor_name, class = data_type_name)
+    }
+    formals(constructor) <- make_args_list(constructor_arguments$arg)
+
+    # set meta information about the constructor
+    class(constructor) <- c("constructor", "function")
+
+    # put the constructor in the binding scope
+    assign(constructor_name, constructor, envir = env)
+}
+
+new_process_constructor_function(rlang::expr(CONS2(car, cdr : llist2)), rlang::expr(llist2), environment())
